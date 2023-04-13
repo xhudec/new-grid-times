f473e4f4c7c5aae1ac5a669e5301b8fa18c57dfb
Lukas Hudec <hudec.luke@gmail.com>
2023-04-13 09:03:30 +0200

    feat: implement responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,54 +1,80 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
     <header>
       <SuperHeader>
         <Row>
-          <ActionGroup>
+          <LeftActionGroup>
             <button>
               <Search size={24} />
             </button>
             <button>
               <Menu size={24} />
             </button>
-          </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
+          </LeftActionGroup>
+          <MainHeader>
+            <Logo />
+          </MainHeader>
+          <RightActionGroup>
+            <RightActionGroupMobileContent>
+              <button>
+                <User size={24} />
+              </button>
+            </RightActionGroupMobileContent>
+            <RightActionGroupDesktopContent>
+              <Button>Subscribe</Button>
+              <SubscriberLink href="#">Already a subscriber?</SubscriberLink>
+            </RightActionGroupDesktopContent>
+          </RightActionGroup>
         </Row>
       </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
     </header>
   );
 };
 
 const SuperHeader = styled.div`
-  padding: 16px 0;
-  background: var(--color-gray-900);
-  color: white;
+  @media ${QUERIES.desktopAndUp} {
+    padding: 16px 0 0;
+    margin-bottom: 80px;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
-  display: flex;
-  justify-content: space-between;
+  padding: 0;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: min-content 1fr;
+
+  grid-template-areas:
+    "left-group right-group"
+    "main-header main-header";
+
+  @media ${QUERIES.desktopAndUp} {
+    grid-template-columns: min-content auto min-content;
+    grid-template-rows: auto;
+    justify-content: space-between;
+
+    grid-template-areas: "left-group main-header right-group";
+  }
 `;
 
 const ActionGroup = styled.div`
   display: flex;
   gap: 24px;
+  padding: 16px;
+
+  color: var(--color-gray-100);
+  background-color: var(--color-gray-900);
 
   /*
     FIX: Remove the inline spacing that comes with
@@ -57,14 +83,65 @@ const ActionGroup = styled.div`
   svg {
     display: block;
   }
+
+  @media ${QUERIES.desktopAndUp} {
+    padding: 0 32px;
+    color: inherit;
+    background-color: transparent;
+  }
+`;
+
+const LeftActionGroup = styled(ActionGroup)`
+  flex: 1;
+  grid-area: left-group;
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
+  margin-top: 32px;
+  margin-bottom: 48px;
+
+  grid-area: main-header;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 32px;
-  margin-bottom: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+`;
+
+const RightActionGroup = styled(ActionGroup)`
+  grid-area: right-group;
+  justify-content: flex-end;
+`;
+
+const RightActionGroupMobileContent = styled.div`¨
+  display: block;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
+`;
+
+const RightActionGroupDesktopContent = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    flex-direction: column;
+    justify-content: inherit;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+const SubscriberLink = styled.a`
+  color: var(--color-gray-900);
+  font-size: 0.875rem;
+  font-style: italic;
+  text-decoration: underline;
+  font-weight: var(--font-weight-normal);
 `;
 
 export default Header;
